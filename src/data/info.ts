b41fce04466d91a609773159f4152d2cb273cd87
Sandeep Singh <snayal50@gmail.com>
2026-05-28 00:17:26 +0530

    Add icons and social data

---
+++ src/data/info.ts
@@ -0,0 +1,9 @@
+export const linkedin = "https://www.linkedin.com/in/sandeepsinghnayal/";
+
+export const twitter = "https://twitter.com/thisMeSandeep";
+
+export const github = "https://github.com/sandeepsinghnayal";
+
+export const email = "mailto:sandeepnayal50@gmail.com";
+
+export const phone = "tel:+919999999999";
